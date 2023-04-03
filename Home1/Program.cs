c275dbdb758e3dcfa3815322486e3ead679ddc29
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2023-04-03 12:43:07 +0300

    Восстановлено ДЗ1

--- Home1/Program.cs
+++ Home1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Home1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your username.");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}!");
+            Console.WriteLine("Please, enter any for close.");
+            Console.ReadLine();
+        }
+    }
+}
