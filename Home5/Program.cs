15d2da3aa02f2db7a9008d8277f103b4dd3cf524
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2023-04-14 15:56:08 +0300

    Adding homework 5

---
+++ Home5/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Home5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AcademicProgressEvaluation academicProgressEvaluation = new AcademicProgressEvaluation();
+
+            Random random = new Random();
+
+            //Допустим есть 3 группы (Group1, Group2, Group3). Создайте 3 массива студентов по 5 в каждой группе.
+            Student[] Group1 = Student.GenerateStudentsArray(5, nameof(Group1));
+            Student[] Group2 = Student.GenerateStudentsArray(5, nameof(Group2));
+            Student[] Group3 = Student.GenerateStudentsArray(5, nameof(Group3));
+
+            List<Student> bestStudents = new List<Student>();
+
+            List<Student[]> groupList = new List<Student[]>
+            {
+                Group1,
+                Group2,
+                Group3
+            };
+
+            //Оценки по дисциплинам задайте с использованием Random
+            foreach(Student[] group in groupList) 
+            {
+                Student.SetRandomMark(group);
+            }
+
+            /*Создайте метод вывода в консоль студента из каждой группы с наилучшей оценкой по Математике. Если существуют студенты с одинаковыми наилучшими оценками - выведите любого из них. (ex: Anton, Math mark: 10)
+              Создайте метод вывода в консоль студента из каждой группы с наилучшей оценкой по Физкультуре. Если существуют студенты с одинаковыми наилучшими оценками - выведите любого из них.
+              Создайте метод вывода в консоль студента из каждой группы с наилучшей оценкой по Биологии. Если существуют студенты с одинаковыми наилучшими оценками - выведите любого из них.*/
+
+            foreach(Student[] group in groupList)
+            {
+                bestStudents.Add(academicProgressEvaluation.GetStudentWithBestMark(group, "Math"));
+                bestStudents.Add(academicProgressEvaluation.GetStudentWithBestMark(group, "Biology"));
+                bestStudents.Add(academicProgressEvaluation.GetStudentWithBestMark(group, "PhysicalEducation"));
+            }
+
+            //Установите каждому из лучших студентов выше - денежное вознаграждение в рублях - reward (rand: 0 - 100) (предусмотрите, чтобы нельзя было установить значение reward < 1 рубля)
+            foreach(Student student in bestStudents)
+            {
+                student.SetReward(random.Next(0,Constants.maxReward));
+            }
+
+            //Создайте методы подсчета и вывода среднего балла группы студентов по каждой из дисциплин и вывода этой информации в консоль (разные методы) (ex: Group1, avarage mark math: 8.3)
+            foreach (Student[] group in groupList)
+            {
+                academicProgressEvaluation.DisplayAverageMathMark(group);
+                academicProgressEvaluation.DisplayAverageBiologyMark(group);
+                academicProgressEvaluation.DisplayAveragePhysicalEducationMark(group);
+            }
+
+            //Создайте метод расчета среднего балла группы студентов по всем 3 дисциплинам (средняя оценка группы по каждой дисциплине / количество дисциплин) и выведите эту информацию в консоль. 
+            //(Average mark of Group1 - Math, PhysicalEducation, Biology - 8.3)
+            foreach (Student[] group in groupList)
+            {
+                academicProgressEvaluation.DisplayAverageMarkByGroup(group);
+            }
+
+            //Добавьте каждому студенту из группы с наибольшим средним баллом по всем дисциплинам произвольный reward.
+            academicProgressEvaluation.SetRewardToStudentsInGroupWithBestAverageMark(groupList);
+
+            //Выведите на экран студента с наибольшим reward. Если таких студентов несколько - выведите их всех.
+            academicProgressEvaluation.DisplayStudentsWithMaxReward(groupList);
+        }
+    }
+}
---
+++ Home5/Student.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home5
+{
+    public class Student
+    {
+        public string Id;
+        public string Name;
+        public int Age;
+        public string Group;
+        public int MathMark;
+        public int PhysicalEducationMark;
+        public int BiologyMark;
+        public int Reward;
+
+        private static string[] someNames = new string[] { "Иван", "Олег", "Анна", "Денис", "Дарья", "Елена", "Максим", "Алексей", "Фёдор", "Мирон", "Наталья", "Николай", "Александр", "Марина" };
+
+        public Student(string id, string name, int age, string group)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Group = group;
+        }
+        public void SetMathMark(int mark)
+        {
+            if (mark < Constants.minMark)
+            {
+                MathMark = Constants.minMark;
+            }
+            else if (mark > Constants.maxMark)
+            {
+                MathMark = Constants.maxMark;
+            }
+            else
+            {
+                MathMark = mark;
+            }
+        }
+        public void SetPhysicalEducationMark(int mark)
+        {
+            if (mark < Constants.minMark)
+            {
+                PhysicalEducationMark = Constants.minMark;
+            }
+            else if (mark > Constants.maxMark)
+            {
+                PhysicalEducationMark = Constants.maxMark;
+            }
+            else
+            {
+                PhysicalEducationMark = mark;
+            }
+        }
+        public void SetBiologyMark(int mark)
+        {
+            if (mark < Constants.minMark)
+            {
+                BiologyMark = Constants.minMark;
+            }
+            else if (mark > Constants.maxMark)
+            {
+                BiologyMark = Constants.maxMark;
+            }
+            else
+            {
+                BiologyMark = mark;
+            }
+        }
+        public void SetReward(int reward)
+        {
+            if (reward < Constants.minReward)
+            {
+                Reward = Constants.minReward;
+            }
+            else if (reward > Constants.maxReward)
+            {
+                Reward = Constants.maxReward;
+            }
+            else
+            {
+                Reward = reward;
+            }
+        }
+        public static Student[] GenerateStudentsArray(int count, string groupName)
+        {
+            Random random = new Random();
+            Student[] studentsArray = new Student[count];
+            for (int i = 0; i < count; i++)
+            {
+                studentsArray[i] = new Student(System.Guid.NewGuid().ToString(), someNames[random.Next(0, someNames.Length)], random.Next(18, 22), groupName);
+            }
+            return studentsArray;
+        }
+        public static void SetRandomMark(Student[] studentsArray)
+        {
+            Random random = new Random();
+            foreach (Student student in studentsArray)
+            {
+                student.SetMathMark(random.Next(Constants.minMark, Constants.maxMark));
+                student.SetBiologyMark(random.Next(Constants.minMark, Constants.maxMark));
+                student.SetPhysicalEducationMark(random.Next(Constants.minMark, Constants.maxMark));
+            }
+        }
+    }
+}
