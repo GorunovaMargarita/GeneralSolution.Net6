d54eaf159e5d334036faa457fd76a976c50a189e
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2023-04-23 19:44:30 +0300

    Adding Homework8

---
+++ Home8/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home8
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int MaxNumberOfCalories { get; set; }
+        public Person(string name, int maxNumberOfCalories) 
+        {
+            Name = name;
+            MaxNumberOfCalories = maxNumberOfCalories;
+        }
+        /// <summary>
+        /// Method for check ration for person. Remove productd from list for day, if sum of calories more than MaxNumberOfCalories for person
+        /// </summary>
+        /// <param name="rationForSomePerson">ration</param>
+        /// <param name="person">person</param>
+        public void CheckAndUpdateRation(Dictionary<DayOfWeek, List<Product>> rationForSomePerson, Person person)
+        {
+            foreach (KeyValuePair<DayOfWeek, List<Product>> pair in rationForSomePerson)
+            {
+                int sumCalories = 0;
+                foreach (Product product in pair.Value)
+                {
+                    sumCalories += product.NumberOfCalories;
+                }
+                if (sumCalories > person.MaxNumberOfCalories)
+                {
+                    do
+                    {
+                        Product productForDelete = pair.Value.First();
+                        Console.WriteLine($"Remove. Day: {pair.Key}, product: {productForDelete}");
+                        pair.Value.Remove(productForDelete);
+                        sumCalories -= productForDelete.NumberOfCalories;
+                    }
+                    while(sumCalories > person.MaxNumberOfCalories);
+                }
+            }
+        }
+    }
+}
---
+++ Home8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Home8 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person somePerson = new Person("Ivan", 1000);
+            RationCreator creator = new RationCreator();
+
+            Dictionary<DayOfWeek, List<Product>> rationForSomePerson = creator.NewRation(somePerson);
+
+            RationCreator.ShowRation(rationForSomePerson);
+
+            somePerson.CheckAndUpdateRation(rationForSomePerson, somePerson);
+            
+            RationCreator.ShowRation(rationForSomePerson);
+        }
+    }
+}
