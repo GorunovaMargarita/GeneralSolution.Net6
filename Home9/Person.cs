1ca59a30ca1dfb367f6326d854cf1b3b80d90096
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2023-04-28 18:00:31 +0300

    Adding homework 9

---
+++ Home9/Person.cs
@@ -0,0 +1,58 @@
+﻿using Home9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        private int age;
+        private int salary;
+        public int Age 
+        {
+            get { return age; }
+            set 
+            { 
+                if(value < 18)
+                {
+                    throw new AgeException("Person is yanger than 18.");
+                }
+                else
+                {
+                    age = value;
+                }
+            } 
+        }
+        public int Salary 
+        { 
+            get { return salary; }
+            set
+            {
+                if (value < 100)
+                {
+                    throw new SalaryException("Salary is smaller than 100.");
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+        public Person(string name, int age, int salary) 
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, age: {Age}, salary: {Salary}";
+        }
+    }
+}
---
+++ Home9/PersonDataGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    public class PersonDataGenerator
+    {
+        private static string[] someNames = new string[] { "Иван", "Олег", "Анна", "Денис", "Дарья", "Елена", "Максим", "Алексей", "Фёдор", "Мирон", "Наталья", "Николай", "Александр", "Марина" };
+        public List<Person> GeneratePersonList(int personCount)
+        {
+            var personList = new List<Person>();
+            for(int i = 0; i < personCount; i++)
+            {
+                personList.Add(new Person(someNames[new Random().Next(0, someNames.Length)],new Random().Next(18,60),new Random().Next(100,1500)));
+            }
+            return personList;
+        }
+    }
+}
